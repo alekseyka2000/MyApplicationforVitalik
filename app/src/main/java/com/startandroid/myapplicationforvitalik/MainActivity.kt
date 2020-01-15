fcbbe18d6d8cd3fe4bae7bcd4302e1158c460256
Aleksey <alekseyka2000@gmail.com>
2020-01-15 18:21:19 +0300

    second

--- app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
+++ app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
@@ -3,27 +3,48 @@ package com.startandroid.myapplicationforvitalik
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.inTransaction {
+            add(R.id.frame, HomeFragment())
+        }
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        fun replaceFragment(fragment: Fragment ){
+            supportFragmentManager.inTransaction {
+                replace(R.id.frame, fragment)
+            }
+        }
+        val listener = navView.setOnNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+               R.id.navigation_home ->{replaceFragment(HomeFragment())
+                   return@setOnNavigationItemSelectedListener true
+               }
+                R.id.navigation_dashboard ->{replaceFragment(DashboardFragment())
+                   return@setOnNavigationItemSelectedListener true
+               }
+                R.id.navigation_notifications ->{replaceFragment(NotificationFragment())
+                   return@setOnNavigationItemSelectedListener true
+               }
+                R.id.navigation_calls ->{replaceFragment(CallsFragment())
+                   return@setOnNavigationItemSelectedListener true
+               }
+                R.id.navigation_contacts ->{replaceFragment(ContactsFragment())
+                   return@setOnNavigationItemSelectedListener true
+               }
+                else -> false
+            }
+        }
     }
+}
+
+private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
 }
