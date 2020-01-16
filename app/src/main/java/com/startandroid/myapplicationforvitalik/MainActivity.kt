73317407e4855806fc8170870d94cd70b4cedb4e
Aleksey <alekseyka2000@gmail.com>
2020-01-16 10:36:55 +0300

    third

--- app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
+++ app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
@@ -1,42 +1,64 @@
 package com.startandroid.myapplicationforvitalik
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var currentFragment: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.inTransaction {
-            add(R.id.frame, HomeFragment())
+            add(R.id.frame, HomeFragment(),"one")
         }
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        fun replaceFragment(fragment: Fragment ){
+        currentFragment= "one"
+
+        fun createOtherFragment(fragment: Fragment, TAG: String ){
             supportFragmentManager.inTransaction {
-                replace(R.id.frame, fragment)
+                add(R.id.frame, fragment,TAG)
+                detach(fragment)
             }
         }
+
+        createOtherFragment(DashboardFragment(),"two")
+        createOtherFragment(NotificationFragment(),"three")
+        createOtherFragment(CallsFragment(),"four")
+        createOtherFragment(ContactsFragment(),"five")
+
+        fun showFragment(s:String){
+            supportFragmentManager.inTransaction {
+            detach(supportFragmentManager.findFragmentByTag(currentFragment)!!)
+            attach(supportFragmentManager.findFragmentByTag(s)!!)
+            }
+            currentFragment=s
+        }
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
         val listener = navView.setOnNavigationItemSelectedListener { menuItem ->
             when(menuItem.itemId){
-               R.id.navigation_home ->{replaceFragment(HomeFragment())
+               R.id.navigation_home ->{showFragment("one")
                    return@setOnNavigationItemSelectedListener true
                }
-                R.id.navigation_dashboard ->{replaceFragment(DashboardFragment())
+                R.id.navigation_dashboard ->{showFragment("two")
                    return@setOnNavigationItemSelectedListener true
                }
-                R.id.navigation_notifications ->{replaceFragment(NotificationFragment())
+                R.id.navigation_notifications ->{showFragment("three")
                    return@setOnNavigationItemSelectedListener true
                }
-                R.id.navigation_calls ->{replaceFragment(CallsFragment())
+                R.id.navigation_calls ->{showFragment("four")
                    return@setOnNavigationItemSelectedListener true
                }
-                R.id.navigation_contacts ->{replaceFragment(ContactsFragment())
+                R.id.navigation_contacts ->{showFragment("five")
                    return@setOnNavigationItemSelectedListener true
                }
                 else -> false
