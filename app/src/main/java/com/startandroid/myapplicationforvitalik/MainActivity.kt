e6df689266c567c439088c7c36aa810f7d674a2b
Aleksey <alekseyka2000@gmail.com>
2020-01-16 10:37:16 +0300

    third

--- app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
+++ app/src/main/java/com/startandroid/myapplicationforvitalik/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.startandroid.myapplicationforvitalik
 
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
