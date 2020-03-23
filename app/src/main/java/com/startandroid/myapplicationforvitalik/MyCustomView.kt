1b0568578fcc72441666577b881298ce8682fb2c
aleks <alekseyka2000@gmail.com>
2020-03-23 16:58:52 +0300

    first commit in developer branch

---
+++ app/src/main/java/com/startandroid/myapplicationforvitalik/MyCustomView.kt
@@ -0,0 +1,14 @@
+package com.startandroid.myapplicationforvitalik
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+class MyCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr){
+
+}
